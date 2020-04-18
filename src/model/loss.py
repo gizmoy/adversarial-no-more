b108517a31dde376935b5fa8067f115cc4ca47bd
Michał Giemza <m.giemza@sigmoidal.io>
2020-04-18 05:35:39 +0200

    apply division epsilon in harmonic mean loss

--- src/model/loss.py
+++ src/model/loss.py
@@ -6,10 +6,10 @@ def cross_entropy_loss(output, target):
     return F.cross_entropy(output, target)
 
 
-def harmonic_mean_loss(output):
+def harmonic_mean_loss(output, eps=1e-6):
     B, C = output.shape
     probs = F.softmax(output, dim=1)
-    batch_losses = - C / torch.sum(probs.pow(-1), dim=1)
+    batch_losses = - C / torch.sum((probs + eps).pow(-1), dim=1)
     return C * torch.sum(batch_losses) / B
 
 
